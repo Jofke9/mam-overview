5f01979beeba286b6769f4f985138a0b68077fd4
Josse Callens <josse.callens@ugent.be>
2023-03-26 16:14:03 +0200

    add: Better visibility and sorting of units

--- src/components/Builder.jsx
+++ src/components/Builder.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import Unit from './Unit.jsx';
+import Units from './Units.jsx';
+import Unit from './Unit.jsx'
 
 export default function Builder() {
-    const [selectedBuilder, setSelectedBuilder] = useState(undefined); // state for the selected builder
+    const [selectedBuilder, setSelectedBuilder] = useState(null); // state for the selected builder
 
     const builders = [
         {
@@ -27,8 +28,163 @@ export default function Builder() {
         },
     ];
 
+    const baseUnits = [
+        {
+            type: "melee",
+            units: [
+                {
+                    name: "Beholder",
+                    img: "../images/units/BaseUnits/beholder.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Rexxar",
+                    img: "../images/units/BaseUnits/rexxar.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Angel",
+                    img: "../images/units/BaseUnits/angel.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Anubis",
+                    img: "../images/units/BaseUnits/anubis.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+            ],
+        },
+        {
+            type: "ranged",
+            units: [
+                {
+                    name: "Marine",
+                    img: "../images/units/BaseUnits/marine.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Stalker",
+                    img: "../images/units/BaseUnits/stalker.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Ghost",
+                    img: "../images/units/BaseUnits/ghost.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Sun Idol",
+                    img: "../images/units/BaseUnits/sun-idol.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+            ],
+        },
+        {
+            type: "magical",
+            units: [
+                {
+                    name: "High Templar",
+                    img: "../images/units/BaseUnits/high-templar.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Snow Queen",
+                    img: "../images/units/BaseUnits/snow-queen.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Arc",
+                    img: "../images/units/BaseUnits/arc.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Conjurer",
+                    img: "../images/units/BaseUnits/conjurer.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0, type: "Regular" },
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0, type: "Reza" }
+                    ]
+                },
+            ],
+        },
+        {
+            type: "mechanical",
+            units: [
+                {
+                    name: "Berserker",
+                    img: "../images/units/BaseUnits/berserker.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Butterfly",
+                    img: "../images/units/BaseUnits/butterfly.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Battle tank",
+                    img: "../images/units/BaseUnits/battletank.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+                {
+                    name: "Robotron",
+                    img: "../images/units/BaseUnits/robotron.png",
+                    cost: { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 },
+                    upgradeCosts: [
+                        { gold: 0, food: 0, metal: 0, mana: 0, oil: 0, crystal: 0, subdolak: 0 }
+                    ]
+                },
+            ],
+        },
+    ];
+
+
     const handleBuilderChange = (event) => {
-        setSelectedBuilder(event.target.value);
+        setSelectedBuilder(builders.find((builder) => builder.id === parseInt(event.target.value)));
     };
 
     return (
@@ -38,7 +194,7 @@ export default function Builder() {
                 <select
                     id="builder"
                     className="block appearance-none w-full py-2 px-3 border border-gray-400 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out"
-                    value={selectedBuilder}
+                    value={selectedBuilder?.id || ''}
                     onChange={handleBuilderChange}
                 >
                     <option value="">Select a builder...</option>
@@ -48,16 +204,37 @@ export default function Builder() {
                         </option>
                     ))}
                 </select>
+                {/* Builder with its units */}
                 {selectedBuilder && (
-                    <div className="mt-4">
+                    <div className="mt-4 flex flex-col items-center">
+                        <p className="text-sm font-medium mt-2">{selectedBuilder.name}</p>
                         <img
-                            src={builders[selectedBuilder - 1]?.img}
-                            alt={`Builder ${selectedBuilder}`}
-                            className="w-40 h-40 object-cover rounded-md shadow-md" de
+                            src={selectedBuilder.img}
+                            alt={selectedBuilder.name}
+                            className="w-20 rounded-md shadow-md mb-2"
                         />
-                        <Unit builderId={selectedBuilder} /> {/* render the Unit component with the selected builder */}
+                        <Units builderId={selectedBuilder.id} />
                     </div>
                 )}
+
+                {/* All base units */}
+                <div className="mt-4">
+                    <h2 className="text-lg font-medium mb-2">Units:</h2>
+                    <div className="grid grid-cols-4 gap-4">
+                        {baseUnits.map((unitGroup) => (
+                            <div key={unitGroup.type}>
+                                <h3 className="text-md font-medium mb-2">{unitGroup.type}</h3>
+                                <div className="grid gap-4">
+                                    {unitGroup.units.map((unit) => (
+                                        <div key={unit.name} className="">
+                                            <Unit name={unit.name} img={unit.img} />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Unit.jsx
+++ src/components/Unit.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Unit() {
+export default function Unit({ name, img }) {
     return (
-        <div></div>
+        <>
+            <p className="text-sm font-medium mt-2 mr-10">{name}</p>
+            <img
+                src={img}
+                alt={name}
+                className="max-h-20 object-contain rounded-md shadow-md"
+            />
+        </>
     )
 }
